13292bd75acbc29c8961afd7f6cf48bbe6a8a8ec
Ian Aranha <ianorama@gmail.com>
2019-08-16 07:59:42 -0700

    Adding Node Folder and first Node server app

---
+++ Node_server/app.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+    
+    // this is how we do routing:
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });     
+    }
+    else if (request.url === "/dojo.html") {
+        fs.readFile('dojo.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/stylesheets/style.css") {
+        fs.readFile('stylesheets/style.css','utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.write(contents); 
+            response.end();
+        });
+   }
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(5000);
+// print to terminal window
+console.log("Running in localhost at port 5000");
+
